457299c1ff4b4ca48adc25356117ea173eed9cdf
Loltolo-Lesapiti <petrolesapiti@gmail.com>
2023-03-23 05:34:12 +0300

    create devise and devise views

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,13 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :foods, foreign_key: 'user_id'
   has_many :recipes, foreign_key: 'user_id'
-  validates :name, presence: true
+  validates :name, :role, presence: true
+
+  def admin?
+    role == 'admin'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root 'foods#index'
   resources :recipes
   resources :foods
   get 'recipe/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  
 end
