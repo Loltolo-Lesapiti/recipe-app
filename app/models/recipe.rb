612cabe17a38d31900f0ce59af42817bfcaea0f5
Loltolo-Lesapiti <petrolesapiti@gmail.com>
2023-03-25 14:07:31 +0300

    Add recipe validations

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,6 +3,11 @@ class Recipe < ApplicationRecord
   has_many :recipe_foods, dependent: :destroy
   has_many :foods, through: :recipe_foods, dependent: :destroy
 
+  validates :name, presence: true, length: { maximum: 100 }
+  validates :cooking_time, comparison: { greater_than: 0 }, numericality: { only_integer: true }
+  validates :preparation_time, comparison: { greater_than: 0 }, numericality: { only_integer: true }
+  validates :description, length: { maximum: 400 }
+
   scope :public_recipes, -> { where(public: true).order(created_at: :desc) }
 
   def toggle_privacy!
